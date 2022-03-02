1f95e87e3a443e121b8d421574ba8c230506f96a
Iwakin Omotoye <iwakinomotoye@gmail.com>
2022-03-02 17:52:39 +0100

    Automatic commit from Bubble

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -3,18 +3,27 @@ function(instance, properties, context) {
     var biIconEl = document.createElement("i");
     var elWidth = properties.bubble.width();
     var elHeight = properties.bubble.height();
+    var iconName = properties.icon_name;
+    var dynamicName = properties.dynamic_name;
+    var iconColor = properties.icon_color;
     
-    if (properties.dynamic_name) {
-      biIconEl.classList.add("bi-" + properties.dynamic_name);
+    if (dynamicName) {
+      biIconEl.classList.add("bi-" + dynamicName);
     } else {
-      biIconEl.classList.add("bi-" + properties.icon_name);   
+      biIconEl.classList.add("bi-" + iconName);   
     }
-    biIconEl.style.color = properties.icon_color;
+    
+    biIconEl.style.color = iconColor;
     if (elWidth > elHeight) {
       biIconEl.style.fontSize = elHeight + "px";
     } else {
       biIconEl.style.fontSize = elWidth + "px";   
     }
+    
+    if (instance.canvas[0].firstElementChild) {
+        instance.canvas[0].removeChild(instance.canvas[0].firstElementChild);
+    }
+    
 	instance.canvas.append(biIconEl);
 	
 }
